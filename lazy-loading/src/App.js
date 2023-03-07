827f41855a6e377b48f9883c11f9d0ce227fbea1
muneeb706 <muneeb706@gmail.com>
2023-03-06 23:09:22 -0800

    feat(src): Error Boundary
    Error Boundary component example for catching and handling errors.

--- lazy-loading/src/App.js
+++ lazy-loading/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import React, {Suspense} from 'react'
+import ErrorBoundary from './ErrorBoundary';
 const About = React.lazy(() => import('./About'));
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback = {<div> Please Wait ... </div>} >
-        <About/>
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback = {<div> Please Wait ... </div>} >
+          <About/>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }

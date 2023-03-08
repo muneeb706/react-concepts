c70099ecd5ea23c76c939c68ad3958ded416cc44
muneeb706 <muneeb706@gmail.com>
2023-03-07 23:04:35 -0800

    feat(src): Lazy Loading Multiple Components Example
    lazily loaded multiple components and rendered with one Suspense fallback.

--- lazy-loading/src/index.js
+++ lazy-loading/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Suspense, lazy } from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const Artists = lazy(() => import('./Artists'));
+const Performers = lazy(() => import('./Performers'))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+ render(){
+  return(
+    <div className="App">
+      <Suspense fallback={<h1>Still Loading...</h1>}>
+        <Artists />
+        <Performers />
+      </Suspense>
+    </div>
+   );
+ }
+}
+ReactDOM.render(<App />, document.getElementById('root'));

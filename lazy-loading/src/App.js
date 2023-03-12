43e7281a8bc7d64005b9ae2ce0b8530ff350fd1e
muneeb706 <muneeb706@gmail.com>
2023-03-12 11:02:38 -0700

    feat(src): Button Props Validation
    props validation rule added for button component

--- lazy-loading/src/App.js
+++ lazy-loading/src/App.js
@@ -1,23 +1,25 @@
-import Button from './Button';
+import Button from "./Button";
 
 function App() {
-  return <>
-    <div>
-      <Button>Click Here!</Button>
-    </div>
-    <div>
-      <Button></Button>
-    </div>
-    <div>
-      <Button></Button>
-    </div>
-    <div>
-      <Button></Button>
-    </div>
-    <div>
-      <Button></Button>
-    </div>
-  </>;
+  return (
+    <>
+      <div>
+        <Button primary>Component 1</Button>
+      </div>
+      <div>
+        <Button>Component 2</Button>
+      </div>
+      <div>
+        <Button>Component 3</Button>
+      </div>
+      <div>
+        <Button>Component 4</Button>
+      </div>
+      <div>
+        <Button>Component 5</Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- lazy-loading/src/Button.js
+++ lazy-loading/src/Button.js
@@ -1,3 +1,5 @@
+import propTypes from "prop-types";
+
 function Button({
   children,
   primary,
@@ -10,5 +12,23 @@ function Button({
 }) {
   return <button>{children}</button>;
 }
+
+// TODO: Use TS instead of prop types
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only one of primary, secondary, success, warning, danger can be true"
+      );
+    }
+  },
+};
 
 export default Button;
